7eb62e95d9d02e18579d0060f747bfa6faa3691b
Ali Slayman <ali.slayman2010@gmail.com>
2026-05-22 20:26:53 +0000

    test(suggester): zero number of suggestions returns empty result

--- tests/Suggester.Tests/SuggesterTests.cs
+++ tests/Suggester.Tests/SuggesterTests.cs
@@ -14,4 +14,14 @@ public class SuggesterTests
         IEnumerable<string> actualSuggestions = suggester.GetSuggestions("gros", Array.Empty<string>(), 2);
         actualSuggestions.Should().BeEmpty();
     }
+
+    [Fact]
+    public void Zero_number_of_suggestions_returns_empty_result()
+    {
+        IDifferenceScorer differenceScorer = new DifferenceScorer();
+        ISuggester suggester = new Suggester(differenceScorer);
+        string[] availableChoices = new[] { "gros", "gras" };
+        IEnumerable<string> actualSuggestions = suggester.GetSuggestions("gros", availableChoices, 0);
+        actualSuggestions.Should().BeEmpty();
+    }
 }
