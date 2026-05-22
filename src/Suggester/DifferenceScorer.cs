f729c3f5295caf844fa85986b0604ae55452b465
Ali Slayman <ali.slayman2010@gmail.com>
2026-05-22 20:11:48 +0000

    feat(scorer): guard against mismatched lengths with GlobalConst message

--- src/Suggester/DifferenceScorer.cs
+++ src/Suggester/DifferenceScorer.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Suggester;
 
 public sealed class DifferenceScorer : IDifferenceScorer
 {
     public int GetDifferenceScore(string destinationText, string sourceText)
     {
+        if (destinationText.Length != sourceText.Length) throw new ArgumentException(GlobalConst.StringsMustHaveSameLengthMessage, nameof(sourceText));
         int differingCharacterCount = 0;
         for (int characterIndex = 0; characterIndex < destinationText.Length; characterIndex++)
         {
---
+++ src/Suggester/GlobalConst.cs
@@ -0,0 +1,6 @@
+namespace Suggester;
+
+public static class GlobalConst
+{
+    public const string StringsMustHaveSameLengthMessage = "Strings must have the same length.";
+}
